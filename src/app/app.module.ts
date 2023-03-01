d238ff01c9cb0b315124d8270868b1a76d83a473
Antonio Rojas <antonio.rojas.dev@gmail.com>
2023-02-28 21:44:51 -0600

    fin seccion 4 - router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRountingModule } from './app-rounting.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    PagesComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRountingModule
+    AppRountingModule,
+    PagesModule,
+    AuthModule,
   ],
   bootstrap: [AppComponent]
 })
